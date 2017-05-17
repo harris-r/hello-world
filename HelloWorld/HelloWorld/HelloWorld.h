b0a4c8a31c8c630b4f37b4c927ec9a394931834a
Raghuraman Sridhar <raghuraman.sridhar@in.bosch.com>
2017-05-17 17:31:18 -0500

    Initial commit

---
+++ HelloWorld/HelloWorld/HelloWorld.h
@@ -0,0 +1,19 @@
+//
+//  HelloWorld.h
+//  HelloWorld
+//
+//  Created by Raghuraman Sridhar on 5/17/17.
+//  Copyright © 2017 Raghu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloWorld.
+FOUNDATION_EXPORT double HelloWorldVersionNumber;
+
+//! Project version string for HelloWorld.
+FOUNDATION_EXPORT const unsigned char HelloWorldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorld/PublicHeader.h>
+
+
---
+++ HelloWorld/HelloWorld/Main.swift
@@ -0,0 +1,19 @@
+//
+//  Main.swift
+//  HelloWorld
+//
+//  Created by Raghuraman Sridhar on 5/17/17.
+//  Copyright © 2017 Raghu. All rights reserved.
+//
+
+import Foundation
+
+public class Main {
+    public func currentDate() -> Date{
+        return calculateCurrentDate
+    }
+    
+    private func calculateCurrentDate() -> Date {
+        return Date()
+    }
+}
